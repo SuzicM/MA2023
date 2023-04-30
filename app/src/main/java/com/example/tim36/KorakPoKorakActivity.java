f8a31d4ceb5a8f439d94e35404f1ce0e9b81a2be
boki20 <bojan.vucetic9@gmail.com>
2023-04-30 10:31:48 +0200

    Commit 1

---
+++ app/src/main/java/com/example/tim36/KorakPoKorakActivity.java
@@ -0,0 +1,16 @@
+package com.example.tim36;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KorakPoKorakActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_korakpokorak);
+
+
+    }
+}
